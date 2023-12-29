ff233407c8181d1a4104f71b98affef5d0758f15
DerWaldgang <bekamakhaev@gmail.com>
2023-12-29 05:31:19 +0300

    feat: [15] payment form

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,85 +1,34 @@
-import { FormEvent, useState } from "react";
 import styles from "./test.module.scss";
-import { loginThunk, logout, registerThunk } from "@/redux/features/authSlice";
-import { useAppDispatch, useAppSelector, useAuth } from "@/redux/hook";
 import CityModal from "@/components/CityModal/CityModal";
+import { PaymentElement } from "@stripe/react-stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { BASE_URL, STRIPE_PUBLISH_KEY } from "@/utils";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { loadStripe } from "@stripe/stripe-js";
+import PaymentCardForm from "@/components/PaymentCardForm/PaymentCardForm";
+
+const stripe = loadStripe(STRIPE_PUBLISH_KEY);
 
 const Test = () => {
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useAppDispatch();
-  const isAuth = useAuth();
+  const [clientSecret, setClientSecret] = useState("");
 
-  const userID = useAppSelector((state) => state.authSlice.userID);
-  const jwt = useAppSelector((state) => state.authSlice.token);
+  useEffect(() => {
+    axios.post(BASE_URL + "/stripe").then(async (res: any) => {
+      setClientSecret(res?.data?.clientSecret);
+    });
+  }, []);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleSubmitLogin = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(
-      loginThunk({
-        mail: email,
-        password: password,
-      })
-    );
-  };
-
-  const handleSubmitRegister = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(
-      registerThunk({
-        name: username,
-        password,
-        mail: email,
-        address: "",
-        city: "",
-        phone: "",
-      })
-    );
-  };
+  console.log(clientSecret, "clientSecret");
 
   return (
     <div className={styles.test}>
-      <form onSubmit={handleSubmitLogin}>
-        <input
-          value={email}
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          value={password}
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">{isAuth ? "Logout" : "Login"}</button>
-      </form>
-      <p>{userID}</p>
-      <p>{jwt}</p>
-      <button onClick={() => dispatch(logout())}>logout</button>
-
-      <form onSubmit={handleSubmitRegister}>
-        <input
-          value={email}
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          value={username}
-          placeholder="username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          value={password}
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">{isAuth ? "Logout" : "Register"}</button>
-      </form>
-      <p>{userID}</p>
-      <p>{jwt}</p>
-      <button onClick={() => dispatch(logout())}>logout</button>
-      <CityModal />
+      {stripe && clientSecret && (
+        <Elements stripe={stripe} options={{ clientSecret }}>
+          <PaymentCardForm />
+        </Elements>
+      )}
     </div>
   );
 };
